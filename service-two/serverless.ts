31c8d04f396adf1c764d55f3ac09c50616a2d50a
Bruno Damasceno <damascenobdm@gmail.com>
2023-10-13 09:24:44 -0300

    feat: add hardcoded resource id

--- service-two/serverless.ts
+++ service-two/serverless.ts
@@ -1,21 +1,23 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import hello from "@functions/hello";
 
 const serverlessConfiguration: AWS = {
-  service: 'service-two',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "service-two",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
+      restApiId: "mahatnc9cg",
+      restApiRootResourceId: "vcnbhbi7d3",
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
   // import the function via paths
@@ -26,10 +28,10 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
